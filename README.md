3b2f8710441e930501688d96392156282ffb1f1c
Jeff Sandberg <paradox460@gmail.com>
2012-10-26 15:12:59 -0600

    Readme update and seed file

---
+++ README.md
@@ -0,0 +1,12 @@
+The future Urails website!
+
+![Screenshot](http://i.imgur.com/1uy69.png)
+
+# Installation
+This is a simple rails app, and to get it running, I recommend following this proceedure
+
+1. Git clone the app to a directory you want, and navigate to said directory
+2. `bundle install` to get the dependencies
+3. `rake db:setup` to get the databases set up and populated
+4. `rake db:migrate` as a precaution
+5. `rails server`
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,41 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Blogpost.create.tap do |bp|
+  title = "A test blogpost"
+  body = "This is a post of awesome
+
+<!--more-->
+
+# Heading 1
+## Heading 2
+### Heading 3
+#### Heading 4
+##### Heading 5
+###### Heading 6
+
+* List
+* List
+  + List
+      - List
+* Unordered List
+
+1. List
+2. List
+3. Ordered List
+
+> Blockquotes are a lot of fun and they can go on and on and never stop because they rock.
+
+> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+
+```ruby
+puts('Ruby is better than everything!')
+```"
+  published = true
+end.save!
