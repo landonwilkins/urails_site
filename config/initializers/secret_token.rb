34e93e6a3decc4fb784ef83e5acf76dfd6b7b1d2
Jeff Sandberg <paradox460@gmail.com>
2012-10-26 11:47:17 -0600

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+UrailsSite::Application.config.secret_token = '84553f8f0f0f6ffaa2da2b2f8257040a02400715f361aade1b5123ce414c1061309ad13b4fa688fa9ecbb5559622c8dcb682124bae7c4a2d31a1a974060ee367'
