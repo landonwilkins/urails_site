c86e978d90d32d124a8c4db805278670c967dd25
Jeff Sandberg <paradox460@gmail.com>
2012-10-27 11:23:42 -0600

    Linked article, added seed file

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,5 +2,5 @@ class Article < ActiveRecord::Base
   attr_accessible :body, :excerpt, :title
 
   extend FriendlyId
-  friendly_id :name, use: [:slugged, :history]
+  friendly_id :title, use: [:slugged, :history]
 end
